9b9d529b667102c86d7fca339138ee0a2b074085
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-04-26 16:42:46 +0200

    Added buttons and tray component

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,7 +1,13 @@
+import Button from '../Components/Button';
+import BingoTray from '../Components/BingoTray';
+
 function StartPage() {
     return ( 
         <main>
             <h2>StartPage</h2>
+            <Button title='Ny bricka' />
+            <Button title='Editera ' />
+            <BingoTray />
         </main>
      );
 }
