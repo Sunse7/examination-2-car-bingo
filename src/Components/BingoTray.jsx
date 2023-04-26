60648410e9267b97be8bb7eeb412d0450cb47b5d
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-04-26 16:43:00 +0200

    Wip

--- src/Components/BingoTray.jsx
+++ src/Components/BingoTray.jsx
@@ -1,7 +1,16 @@
+import { useSelector } from 'react-redux';
+import BingoCard from './BingoCard';
+
 function BingoTray() {
+    const state = useSelector((state) => state);
+
+    let randomNumber = Math.floor(Math.random() * 27) // 16 randomnummer? som är deras id som sen renderas ut?
+    console.log(randomNumber, 'random');
+
     return ( 
         <section>
             <h2>Bingo Tray</h2>
+            <BingoCard />
         </section>
      );
 }
