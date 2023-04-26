4d9618d4ba5c334b3e42577287f35af64ac5d22f
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-04-26 11:54:00 +0200

    Basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.scss';
+import SplashPage from './pages/SplashPage';
+import StartPage from './pages/StartPage';
+import EditPage from './pages/EditPage';
 
 function App() {
 
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SplashPage />} />
+        <Route path='/startpage' element={<StartPage />} />
+        <Route path='/editpage' element={<EditPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
