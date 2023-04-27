400372f0318b789d9ca11377729f27cc63be54a6
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-04-27 12:19:26 +0200

    setTimeout on splash

--- src/pages/SplashPage.jsx
+++ src/pages/SplashPage.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function SplashPage() {
-    return ( 
-        <main>
-            <h1>Bil Bingo Time!</h1>
-            <img src="./images/camper-van.png" alt="" />
-        </main>
-     );
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/startpage");
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <main>
+      <h1>Bil Bingo Time!</h1>
+      <img src="./images/camper-van.png" alt="" />
+    </main>
+  );
 }
 
 export default SplashPage;
