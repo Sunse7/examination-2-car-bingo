9e033c84948602dc3f90b20a5b50f822c3e36a0e
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-04-27 17:02:52 +0200

    Randomize bingoCards

--- src/Components/BingoTray.jsx
+++ src/Components/BingoTray.jsx
@@ -1,16 +1,22 @@
 import { useSelector } from 'react-redux';
 import BingoCard from './BingoCard';
+import style from './BingoTray.module.scss';
 
 function BingoTray() {
     const state = useSelector((state) => state.bingoItems);
 
-    let randomNumber = Math.floor(Math.random() * state.length) // 16 randomnummer? som är deras id som sen renderas ut?
-    console.log(randomNumber, 'random');
-    
+    let bingoItemsCopy = [...state];
+
+    for (let i = bingoItemsCopy.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [bingoItemsCopy[i], bingoItemsCopy[j]] = [bingoItemsCopy[j], bingoItemsCopy[i]];
+    }
+
     return ( 
-        <section>
-            <h2>Bingo Tray</h2>
-            <BingoCard />
+        <section className={style.container}>
+            {
+                 bingoItemsCopy && bingoItemsCopy.slice(0, 16).map((bingoItem) => <BingoCard key={bingoItem.id} bingoItem={bingoItem} />)
+            }
         </section>
      );
 }
