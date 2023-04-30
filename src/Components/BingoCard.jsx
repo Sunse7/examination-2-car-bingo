23b9e4d6d81a7cfdb3a126e5a37831e2d79b307c
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-04-30 16:27:47 +0200

    Toggle checkmark

--- src/Components/BingoCard.jsx
+++ src/Components/BingoCard.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import style from "./BingoCard.module.scss";
 
 function BingoCard({ bingoItem }) {
+  const [isChecked, setIsChecked] = useState(false);
+  function ToggleChecked() {
+    setIsChecked(!isChecked);
+  }
+
   return (
-    <article className={style.card}>
-      <figure className={style.checkmark}></figure>
+    <article onClick={ToggleChecked} className={style.card}>
+      <figure className={isChecked ? style.checkmark : null}></figure>
         <p className={style.card__info}>{bingoItem.text}</p>
     </article>
   );
