310497ee1966a2cb4d8151104fe685ef2adefe4b
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-05-02 13:22:04 +0200

    Added wrapper

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,19 +1,25 @@
-import Button from '../Components/Button';
-import BingoTray from '../Components/BingoTray';
-import { useNavigate } from 'react-router-dom';
-import style from './StartPage.module.scss';
+import Button from "../Components/Button";
+import BingoTray from "../Components/BingoTray";
+import { useNavigate } from "react-router-dom";
+import style from "./StartPage.module.scss";
 
 function StartPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return ( 
-        <main className={style.wrapper}>
-            <h2>Bil bingo</h2>
-            <Button title='Ny bricka' isBlue={true} />
-            <Button title='Redigera' action={() => navigate('/editpage')} isBlue={false} />
-            <BingoTray />
-        </main>
-     );
+  return (
+    <main className={style.wrapper}>
+      <h2>Bil bingo</h2>
+      <section className={style.wrapper__section}>
+        <Button title="Ny bricka" isBlue={true} />
+        <Button
+          title="Redigera"
+          action={() => navigate("/editpage")}
+          isBlue={false}
+        />
+      </section>
+      <BingoTray />
+    </main>
+  );
 }
 
 export default StartPage;
